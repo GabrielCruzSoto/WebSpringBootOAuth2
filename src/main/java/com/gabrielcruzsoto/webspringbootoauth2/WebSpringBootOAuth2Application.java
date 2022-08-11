a5e6e7360952a997ae0e2b4384bd22b0af2155c7
gabriel.cruz <gabriel.cruz@soho.com.uy>
2022-08-11 11:48:51 -0400

    project initial

---
+++ src/main/java/com/gabrielcruzsoto/webspringbootoauth2/WebSpringBootOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.gabrielcruzsoto.webspringbootoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringBootOAuth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpringBootOAuth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabrielcruzsoto/webspringbootoauth2/WebSpringBootOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielcruzsoto.webspringbootoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringBootOAuth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
